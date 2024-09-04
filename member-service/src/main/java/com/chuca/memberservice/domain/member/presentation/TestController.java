4c4684e1404b369b18afb646ac36d44194418bdf
aeeazip <aeea0520@gmail.com>
2024-09-04 17:27:23 +0900

    fix(#9) : eureka 설정 주석 해제

--- member-service/src/main/java/com/chuca/memberservice/domain/member/presentation/TestController.java
+++ member-service/src/main/java/com/chuca/memberservice/domain/member/presentation/TestController.java
@@ -1,6 +1,8 @@
 package com.chuca.memberservice.domain.member.presentation;
 
+import com.chuca.memberservice.domain.member.domain.entity.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/test-gateway")
+    public String testGateway(@AuthenticationPrincipal Member member) {
+        return member.getGeneralId();
     }
 }
