1684e4e69b67a909af8169f38d4bf8fcb45f132b
aeeazip <aeea0520@gmail.com>
2024-09-12 11:18:18 +0900

    fix(#9) : gateway 타고 member 서비스 호출

--- gateway/src/main/java/com/chuca/gateway/global/filter/AuthorizationHeaderFilter.java
+++ gateway/src/main/java/com/chuca/gateway/global/filter/AuthorizationHeaderFilter.java
@@ -2,16 +2,22 @@ package com.chuca.gateway.global.filter;
 
 import com.chuca.gateway.global.security.JwtProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 // Gateway로 요청 들어올 때 JWT 토큰 유효성 검사하는 필터
 @Component
@@ -30,7 +36,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
     }
 
-    // 사용자의 헤더에 Authorization 값이 없거나 유효한 토큰이 아니라면 사용자에게 권한이 없다는 401 Unauthorized 코드를 반환한다.
+    // 사용자의 헤더에 Authorization 값이 없거나 유효한 토큰이 아니라면 사용자에게 권한이 없다는 401 Unauthorized 코드를 반환
     @Override
     public GatewayFilter apply(Config config) {
         // Prefilter
@@ -48,7 +54,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
             boolean isValid = jwtProvider.validateToken(token);
             if(!isValid) {
-                return OnError(exchange, "Jwt token is not vaild", HttpStatus.UNAUTHORIZED);
+                return OnError(exchange, "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED);
             }
 
             log.info("AuthorizationHeaderFilter End");
@@ -57,25 +63,25 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     }
 
     // Mono, Flux => Spring WebFlux
-    private Mono<Void> OnError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+    private Mono<Void> OnError(ServerWebExchange exchange, String message, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
 
-        log.error(err);
-        return response.setComplete();
+        // 에러 응답 데이터를 Map에 추가
+        Map<String, Object> errorResponse = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "message", message,
+                "path", exchange.getRequest().getURI().getPath(),
+                "status", httpStatus.value()
+        );
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return Mono.fromCallable(() -> objectMapper.writeValueAsBytes(errorResponse))
+                .map(response.bufferFactory()::wrap)
+                .doOnError(e -> log.error("JSON 변환 중 오류 발생: ", e))
+                .flatMap(dataBuffer -> response.writeWith(Mono.just(dataBuffer))); // Mono.just로 감싸기
     }
-//
-//    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
-//        ServerHttpResponse response = exchange.getResponse();
-//        response.setStatusCode(httpStatus);
-//        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-//
-//        // 에러 메시지를 JSON으로 변환
-//        String errorMessage = String.format("{\"error\": \"%s\"}", err);
-//        DataBufferFactory dataBufferFactory = response.bufferFactory();
-//        DataBuffer dataBuffer = dataBufferFactory.wrap(errorMessage.getBytes(StandardCharsets.UTF_8));
-//
-//        log.error(err);
-//        return response.writeWith(Mono.just(dataBuffer));
-//    }
 }
